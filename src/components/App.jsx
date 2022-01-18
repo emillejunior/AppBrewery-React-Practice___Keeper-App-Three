fdaa65b1e79402a2d26f373499019eb53e0edcda
emillejunior <emillejunior@gmail.com>
2022-01-19 06:43:19 +0700

    git checkout -b practice/practice-one git add . git commit -m "first commit"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,95 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
+
+
 function App() {
+  const dummyNotes = [
+    {
+      id: 1,
+      title: "Note 1",
+      content: "Some basic content"
+    },
+    {
+      id: 2,
+      title: "Note 2",
+      content: "Another basic content"
+    },
+    {
+      id: 3,
+      title: "Note 3",
+      content: "More content"
+    }
+  ]
+  const [notes,setNotes] = useState([])
+  
+  function deleteNote(x) {
+    console.log("Deleting note #" + x)
+
+    setNotes((prevNotes)=>{
+      return prevNotes.filter( note => note.id !== x )
+    })
+
+    // setNotes(prevNotes=>{return prevNotes.map((note)=>{
+    //   return {
+    //         id: 1,
+    //         title: "Note 1",
+    //         content: "Some basic content"
+    //       }
+    // })})
+
+    // setNotes(prevNotes=>{return prevNotes.filter((note)=>{
+    //   return note.id != x
+    // })})
+
+  }
+
+  function saveNote(newNote) {
+    console.log("Saving.. ")
+    console.log(newNote)
+    console.log(notes.length)
+    newNote.id = notes.length + 1
+    setNotes((prevNotes)=>{      
+      newNote.id = prevNotes.length + 1
+      return [...prevNotes, newNote]
+    })
+  }
+  
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+
+      <CreateArea action={saveNote} />
+
+      {notes.map((note)=>{
+        return (
+          <Note 
+          key={note.id} 
+          id={note.id} 
+          title={note.title} 
+          content={note.content}
+          action={deleteNote} 
+          />
+        )
+      })}
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+  const [newNote,setNewNote] = useState({})
+
+  function handleChange(e) {
+    // setText(e.target.value)
+    const text = e.target.value
+    if (e.target.name === "title") {
+      console.log("Title is changing")
+      setNewNote((prevState)=>{
+          return {
+          title: text, 
+          content: prevState.content
+          }
+        }
+      )
+    } else if (e.target.name === "content") {
+      console.log("Content is changing")
+      setNewNote((prevState)=>{
+          return {
+          title: prevState.title, 
+          content: text
+          }
+        }
+      )
+    }
+  }
+
+  function handleClick(e) {
+    console.log(e.target.innerHTML)
+    props.action(newNote)
+    setNewNote({})
+    e.preventDefault()
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+
+        <input
+         onChange={handleChange}
+        //  value={newNote.title}
+         name="title"
+         placeholder="Title"
+        />
+
+        <textarea
+         onChange={handleChange}
+        //  value={newNote.content}
+         name="content"
+         placeholder="Take a note..."
+         rows="3"
+        />
+
+        <button onClick={handleClick}>Add</button>
       </form>
     </div>
   );
